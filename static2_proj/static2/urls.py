bba4fc32e2913faf339787b4f0fa43fa9c41c4f8
CA\xucao <xucao@deloitte.ca>
2021-01-23 20:49:18 -0500

    static2 project

--- static2_proj/static2/urls.py
+++ static2_proj/static2/urls.py
@@ -1,4 +1,4 @@
-"""static1 URL Configuration
+"""static2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from appstatic import views
+from app02 import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-
+    # path('',views.home, name='home'),
+    path('', include('app02.urls')),
 ]
